32704f95b2754a2651fb959906d562b80947e813
Дима <xaunofx@gmail.com>
2026-09-14 14:29:36 +0200

    fix: disable webpack dev packfile cache on Windows

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
